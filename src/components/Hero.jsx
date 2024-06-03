ea7ff4bf40f4d13ec2e329a2c1793f76e394ca40
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-06-03 12:37:31 +0330

    Add GSAP Animation to the Hero Section + Utilizing Videos

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,54 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { heroVideo, smallHeroVideo } from '../utils';
+
 const Hero = () => {
-  return <div>Hero</div>;
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+  //dynamically modify the video depending on the width of the screen
+  const handleVideoSrcSet = () => {
+    setVideoSrc(window.innerWidth < 760 ? smallHeroVideo : heroVideo);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleVideoSrcSet);
+    //in "react" you need to clean up event-listeners
+    return () => {
+      window.removeEventListener('resize', handleVideoSrcSet);
+    };
+  }, []);
+
+  useGSAP(() => {
+    gsap.to('#hero', {
+      opacity: 1,
+      delay: 1.5,
+    });
+  }, []);
+
+  return (
+    <section className="relative w-full bg-black nav-height">
+      <div className="flex flex-col w-full h-5/6 flex-center">
+        <p id="hero" className="hero-title">
+          iPhone 15 Pro
+        </p>
+
+        <div>
+          <video
+            className="pointer-events-none"
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc} //re-render the video when the source changes
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
