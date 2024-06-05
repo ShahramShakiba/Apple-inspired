ac9fc897bbd514fe0bd143822683a265df2dd1e9
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-06-05 21:13:47 +0330

    Add View, Environment and lights

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,11 +1,40 @@
+import {
+  Html,
+  OrbitControls,
+  View,
+  PerspectiveCamera,
+} from '@react-three/drei';
+import * as THREE from 'three';
+import Lights from './Lights';
+import { Suspense } from 'react';
+import Iphone from './Iphone';
 
-
-const ModelView = () => {
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationSize,
+  size,
+  item,
+}) => {
   return (
-    <div>
-      model
-    </div>
+    <View
+      index={index}
+      id={gsapType}
+      className={`border-2 border-red-500 w-full h-full ${
+        index === 2
+      } ? 'right-[-100%] : '`}
+    >
+      <ambientLight intensity={0.3} />
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+      <Lights />
+
+      <Suspense fallback={<div>Loading</div>}>
+        <Iphone />
+      </Suspense>
+    </View>
   );
-}
+};
 
 export default ModelView;
