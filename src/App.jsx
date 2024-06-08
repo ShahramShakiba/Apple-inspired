16a9daca24fd6fc6c84ab4305ff4ee3e19aa32fa
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-06-08 22:51:49 +0330

    Try to add Sentry

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import Hero from './components/Hero';
 import Model from './components/Model';
 import Navbar from './components/Navbar';
 import HighLights from './components/HighLights';
+import * as Sentry from '@sentry/react';
 
 const App = () => {
+  return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
   return (
     <main className="bg-black">
       <Navbar />
@@ -14,4 +16,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Sentry.withProfiler(App);
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+// SENTRY
+import * as Sentry from '@sentry/react';
+
+Sentry.init({
+  dsn: 'https://3cf614b79d5a3a27bace7b011850bc2d@o4507397325717504.ingest.us.sentry.io/4507397328273409',
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
